73ffb2dd9c4e33c52c8b73a936a5dcd5c9bf64d8
ParkJong-ic <whddlrs@naver.com>
2023-01-02 22:06:11 +0900

    문제 풀이

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public int solution(int num) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main() {
+
+    }
+}
