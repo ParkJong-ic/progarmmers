5c86d8cbeeec888c4831e8844cb83b4c5bcec88b
ParkJong-ic <whddlrs@naver.com>
2023-01-29 16:01:27 +0900

    feat : Level1 Ex1

---
+++ src/main/java/Level1/Ex1.java
@@ -0,0 +1,22 @@
+package Level1;
+
+public class Ex1 {
+        public String solution(int a, int b) {
+            String[] week = new String[]{"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+            int[] month = new int[]{31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+            int x = 0;
+            for(int i = 0; i < a - 1; i++) {
+                x += month[i];
+            }
+            x += b -1;
+            return week[x % 7];
+        }
+
+    public static void main(String[] args) {
+        Ex1 ex1 = new Ex1();
+        int a = 5;
+        int b = 24;
+        System.out.println(ex1.solution(a, b));
+
+    }
+}
