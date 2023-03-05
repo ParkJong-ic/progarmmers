edc26013c17b4fc01bd2a5a48d629db2085c202b
ParkJong-ic <whddlrs@naver.com>
2023-03-05 15:48:58 +0900

    fix : Ex17 console.log 삭제

--- src/main/Javascript/Level1/Ex17.js
+++ src/main/Javascript/Level1/Ex17.js
@@ -5,7 +5,6 @@ function solution(n) {
         nums.push(n % 10);
         n = Math.floor(n / 10);
     } while (n > 0);
-    console.log(nums);
     for (let i = 0; i < nums.length; i++) {
         answer += nums[i];
     }
